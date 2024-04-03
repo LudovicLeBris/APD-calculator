1d0f717e2fce8ab259ac61a0b33e97f411a6154a
Ludovic Le Bris <ludovic.lebris@gmail.com>
2024-04-03 16:47:14 +0200

    feat(config) adding color theme in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,30 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      'bg-light': '#FFF',
+      'surface-light': '#EEF9FF',
+      'primary-light': '#2EA0FF',
+      'primary-variant-light': '#146CE1',
+      'secondary-light': '#1756B6',
+      'secondary-variant-light': '#142E57',
+      'tertiary-light': '#D9F0FF',
+      'warning-light': '#FF352E',
+      'bg-dark': '#142E57',
+      'surface-dark': '#1756B6',
+      'primary-dark': '#146CE1',
+      'primary-variant-dark': '#2EA0FF',
+      'secondary-dark': '#1B83F5',
+      'secondary-variant-dark': '#59C0FF',
+      'tertiary-dark': '#1756B6',
+      'warning-dark': '#E02914',
+    },
+    extend: {
+      backgroundImage: (theme) => ({
+        'gradient-light': `linear-gradient(to right, #EEF9FF, #FFF)`,
+        'gradient-dark': `linear-gradient(to right, #1756B6, #142E57)`,
+      }),
+    },
   },
   plugins: [],
 }
