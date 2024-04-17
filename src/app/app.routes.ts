7e6fa8fd97c7a04d83f135873bedb920f6cf8590
Ludovic Le Bris <ludovic.lebris@gmail.com>
2024-04-17 13:36:07 +0200

    refactor(routes) adding first route to project list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { projectsResolver } from './shared/project-resolver.service';
+import { ProjectListComponent } from './apd/project-list/project-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: ProjectListComponent,
+    resolve: {projects: projectsResolver}
+  },
+];
