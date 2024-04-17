64f677487a7c22db212ee9e962ad209ff85eb9c2
Ludovic Le Bris <ludovic.lebris@gmail.com>
2024-04-17 13:32:12 +0200

    refactor(component) adding dynamic data from project

--- src/app/ui/project-card/project-card.component.ts
+++ src/app/ui/project-card/project-card.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { MoreButtonComponent } from '../more-button/more-button.component';
 import { EditButtonComponent } from '../edit-button/edit-button.component';
+import { Project } from '../../apd/shared/models/project.model';
 
 @Component({
   selector: 'app-project-card',
@@ -12,10 +13,18 @@ import { EditButtonComponent } from '../edit-button/edit-button.component';
   templateUrl: './project-card.component.html',
   styleUrl: './project-card.component.css'
 })
-export class ProjectCardComponent {
+export class ProjectCardComponent implements OnInit {
+  @Input() project!: Project;
   // TODO : 36 char max
-  projectName: string = 'Test';
+  name: string = '';
   ductNetworkQuantity: number = 0;
-  projectAltitude: number = 0;
-  projectTemperature: number = 20;
+  altitude: number = 0;
+  temperature: number = 20;
+
+  ngOnInit(): void {
+    this.name = this.project.name;
+    this.ductNetworkQuantity = this.project.ductNetworks.length;
+    this.altitude = this.project.generalAltitude.getValue() as number;
+    this.temperature = this.project.generalTemperature.getValue() as number;
+  }
 }
