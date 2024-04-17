3d45ccf92c55a0e45cd0e7e68855060e6ae3f65d
Ludovic Le Bris <ludovic.lebris@gmail.com>
2024-04-17 20:23:31 +0200

    style(service) fix project service variable name

--- src/app/shared/project-resolver.service.ts
+++ src/app/shared/project-resolver.service.ts
@@ -2,7 +2,6 @@ import { inject } from '@angular/core';
 import { ActivatedRouteSnapshot, ResolveFn, RouterStateSnapshot } from '@angular/router';
 import { JsonProject } from '../apd/shared/models/project.model';
 import { ProjectService } from '../apd/shared/api/project.service';
-import { Observable, first, map, mergeMap, of, take } from 'rxjs';
 import { Errormessage } from '../types/error-message';
 
 
@@ -10,7 +9,7 @@ export const projectsResolver: ResolveFn<{message: string, content: JsonProject[
   route: ActivatedRouteSnapshot,
   state: RouterStateSnapshot,
 ) => {
-  const projecService = inject(ProjectService);
+  const projectService = inject(ProjectService);
 
-  return projecService.getProjects();
+  return projectService.getProjects();
 }
