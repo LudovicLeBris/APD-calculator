d0c60f2b88dbef91a746d23c3465869a62d6a2a5
Ludovic Le Bris <ludovic.lebris@gmail.com>
2024-04-19 11:20:39 +0200

    refactor(component) adding real inputs in more button
    Adapting project card component at the same time

--- src/app/ui/more-button/more-button.component.ts
+++ src/app/ui/more-button/more-button.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { DuctNetwork } from '../../apd/shared/models/duct-network.model';
+import { Project } from '../../apd/shared/models/project.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-more-button',
   standalone: true,
   imports: [],
   template: `
-    <div class="w-14 h-6 button btn-tertiary-light dark:btn-tertiary-dark">
+    <div (click)="goToUrl()" class="w-14 h-6 button btn-tertiary-light dark:btn-tertiary-dark">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
         <path fill-rule="evenodd" d="M4.5 12a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Zm6 0a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Zm6 0a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z" clip-rule="evenodd" />
       </svg>
@@ -14,5 +17,14 @@ import { Component } from '@angular/core';
   styleUrl: './more-button.component.css'
 })
 export class MoreButtonComponent {
+  @Input() url: string = '';
+  @Input() apdEntity: Project | DuctNetwork | undefined;
 
+  constructor(
+    private router: Router,
+  ) {}
+
+  goToUrl() {
+    this.router.navigate([this.url], {state: this.apdEntity})
+  }
 }
