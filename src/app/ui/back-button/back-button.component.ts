d133da603653455707b20021198ffaab10efa6d6
Ludovic Le Bris <ludovic.lebris@gmail.com>
2024-04-19 11:24:46 +0200

    refactor(component) adding real behavior on back button

--- src/app/ui/back-button/back-button.component.ts
+++ src/app/ui/back-button/back-button.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-back-button',
   standalone: true,
   imports: [],
   template: `
-    <div class="size-8 button btn-primary-light dark:btn-primary-dark">
+    <div (click)="goBack()" class="size-8 button btn-primary-light dark:btn-primary-dark">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5">
         <path fill-rule="evenodd" d="M9.53 2.47a.75.75 0 0 1 0 1.06L4.81 8.25H15a6.75 6.75 0 0 1 0 13.5h-3a.75.75 0 0 1 0-1.5h3a5.25 5.25 0 1 0 0-10.5H4.81l4.72 4.72a.75.75 0 1 1-1.06 1.06l-6-6a.75.75 0 0 1 0-1.06l6-6a.75.75 0 0 1 1.06 0Z" clip-rule="evenodd" />
       </svg>
@@ -14,5 +15,11 @@ import { Component } from '@angular/core';
   styleUrl: './back-button.component.css'
 })
 export class BackButtonComponent {
+  constructor(
+    private router: Router,
+  ) {}
 
+  goBack() {
+    this.router.navigate(['..'])
+  }
 }
