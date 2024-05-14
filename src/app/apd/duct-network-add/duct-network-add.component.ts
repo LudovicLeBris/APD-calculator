16274d4e818c0e046d3af6641d79850a5e42f558
Ludovic Le Bris <ludovic.lebris@gmail.com>
2024-05-14 21:19:37 +0200

    feat(component) adding duct network form

--- src/app/apd/duct-network-add/duct-network-add.component.ts
+++ src/app/apd/duct-network-add/duct-network-add.component.ts
@@ -4,12 +4,14 @@ import { JsonProject, Project } from '../shared/models/project.model';
 import { DuctNetwork } from '../shared/models/duct-network.model';
 import { ActivatedRoute } from '@angular/router';
 import { ProjectService } from '../shared/api/project.service';
+import { DuctNetworkFormComponent } from '../duct-network-form/duct-network-form.component';
 
 @Component({
   selector: 'app-duct-network-add',
   standalone: true,
   imports: [
     BackButtonComponent,
+    DuctNetworkFormComponent
   ],
   template: `
     <section class="flex justify-between items-start m-2 border rounded-lg border-primary-light dark:border-primary-dark dark:text-bg-light">
@@ -22,8 +24,8 @@ import { ProjectService } from '../shared/api/project.service';
         <app-back-button [url]="['projets', project.id]"></app-back-button>
       </div>
     </section>
-    <section>
-      <!-- <app-duct-network-form [ductNetwork]="ductNetwork" [Project]="Project"></app-duct-network-form> -->
+    <section class="flex flex-col gap-2 m-2">
+      <app-duct-network-form [ductNetwork]="ductNetwork" [project]="project"></app-duct-network-form>
     </section>
   `,
   styleUrl: './duct-network-add.component.css'
