fc80ea7b0391ac9f43a4721bd53d297d8e50bd6d
Ludovic Le Bris <ludovic.lebris@gmail.com>
2024-05-24 09:36:44 +0200

    feat(routing) adding guard for authentification

---
+++ src/app/shared/auth.guard.ts
@@ -0,0 +1,17 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from './auth.service';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  console.log(authService.isLogged);
+
+  if (authService.isLogged) {
+    return true;
+  }
+
+  router.navigate(['login']);
+  return false;
+
+};
