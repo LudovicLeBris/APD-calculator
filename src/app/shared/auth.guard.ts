1ed59aa7d296edf2e05407b20df4baa4df95553c
Ludovic Le Bris <ludovic.lebris@gmail.com>
2024-05-24 14:39:09 +0200

    fix(guard) delete log

--- src/app/shared/auth.guard.ts
+++ src/app/shared/auth.guard.ts
@@ -5,7 +5,6 @@ import { AuthService } from './auth.service';
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
-  console.log(authService.isLogged);
 
   if (authService.isLogged) {
     return true;
