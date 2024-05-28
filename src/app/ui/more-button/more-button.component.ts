c74f8be9e5b935022287ef22c789212105c2063a
Ludovic Le Bris <ludovic.lebris@gmail.com>
2024-05-28 20:26:49 +0200

    fix(component) adding cursor on ui buttons

--- src/app/ui/more-button/more-button.component.ts
+++ src/app/ui/more-button/more-button.component.ts
@@ -9,7 +9,7 @@ import { DuctSection } from '../../apd/shared/models/duct-section.model';
   standalone: true,
   imports: [],
   template: `
-    <div (click)="goToUrl()" class="w-14 h-6 button btn-tertiary-light dark:btn-tertiary-dark">
+    <div (click)="goToUrl()" class="w-14 h-6 button btn-tertiary-light dark:btn-tertiary-dark cursor-pointer">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
         <path fill-rule="evenodd" d="M4.5 12a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Zm6 0a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Zm6 0a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z" clip-rule="evenodd" />
       </svg>
